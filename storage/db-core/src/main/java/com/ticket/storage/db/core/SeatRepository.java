aa4aba33413186384dd5fdd645a93ef26abde4b8
mn040820 <mn040820@naver.com>
2025-11-30 13:26:15 +0900

    test/feat(performance): 회차의 재고가 0 이하라면 예매 가능한 전체 좌석 조회는 실패한다.

--- storage/db-core/src/main/java/com/ticket/storage/db/core/SeatRepository.java
+++ storage/db-core/src/main/java/com/ticket/storage/db/core/SeatRepository.java
@@ -3,4 +3,5 @@ package com.ticket.storage.db.core;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SeatRepository extends JpaRepository<SeatEntity, Long> {
+    Long countByPerformanceIdAndStatus(Long performanceId, String status);
 }
