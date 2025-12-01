12ff3f33f9ddb96a785e3d42cf3fb1e940246c11
mn040820 <mn040820@naver.com>
2025-12-01 21:28:02 +0900

    fix(performance): 구조, 설계 변경으로 인한 코드 삭제, 변경

--- core/core-api/src/main/java/com/ticket/core/domain/seat/SeatService.java
+++ core/core-api/src/main/java/com/ticket/core/domain/seat/SeatService.java
@@ -1,8 +1,5 @@
 package com.ticket.core.domain.seat;
 
-import com.ticket.core.support.exception.ErrorType;
-import com.ticket.core.support.exception.NotFoundException;
-import com.ticket.storage.db.core.SeatEntity;
 import com.ticket.storage.db.core.SeatRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +12,4 @@ public class SeatService {
         this.seatRepository = seatRepository;
     }
 
-    public Seat findById(final Long id) {
-        final SeatEntity seatEntity = seatRepository.findById(id).orElseThrow(() -> new NotFoundException(ErrorType.NOT_FOUND));
-        return new Seat(seatEntity.getId(), seatEntity.getPerformanceId(), seatEntity.getX(), seatEntity.getY(), seatEntity.getStatus());
-    }
 }
