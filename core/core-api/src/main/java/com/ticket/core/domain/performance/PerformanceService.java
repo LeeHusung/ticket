e0efe33ead28077fecdf4fd3276520c09d9e4666
mn040820 <mn040820@naver.com>
2025-12-01 21:28:02 +0900

    fix(performance): 구조, 설계 변경으로 인한 코드 삭제, 변경

--- core/core-api/src/main/java/com/ticket/core/domain/performance/PerformanceService.java
+++ core/core-api/src/main/java/com/ticket/core/domain/performance/PerformanceService.java
@@ -1,16 +1,8 @@
 package com.ticket.core.domain.performance;
 
-import com.ticket.core.domain.seat.Seat;
-import com.ticket.core.support.exception.CoreException;
-import com.ticket.core.support.exception.ErrorType;
-import com.ticket.core.support.exception.NotFoundException;
 import com.ticket.storage.db.core.PerformanceRepository;
-import com.ticket.storage.db.core.SeatEntity;
 import com.ticket.storage.db.core.SeatRepository;
-import com.ticket.storage.db.core.SeatStatus;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class PerformanceService {
@@ -23,20 +15,21 @@ public class PerformanceService {
         this.seatRepository = seatRepository;
     }
 
-    public List<Seat> findAllSeatByPerformance(final Long performanceId) {
-        final Long availableCount = seatRepository.countByPerformanceIdAndStatus(performanceId, SeatStatus.AVAILABLE);
-        if (availableCount <= 0) throw new CoreException(ErrorType.NOT_EXIST_AVAILABLE_SEAT);
-        final List<SeatEntity> seatEntities = seatRepository.findByPerformanceId(performanceId);
-        if (seatEntities.isEmpty()) {
-            throw new NotFoundException(ErrorType.NOT_FOUND);
-        }
-        return seatEntities.stream()
-                .map(e -> new Seat(e.getId(),
-                        e.getPerformanceId(),
-                        e.getX(),
-                        e.getY(),
-                        e.getStatus())
-                )
-                .toList();
-    }
+    //TODO PerformanceSeat으로 이동 필요
+//    public List<Seat> findAllSeatByPerformance(final Long performanceId) {
+//        final Long availableCount = seatRepository.countByPerformanceIdAndStatus(performanceId, SeatStatus.AVAILABLE);
+//        if (availableCount <= 0) throw new CoreException(ErrorType.NOT_EXIST_AVAILABLE_SEAT);
+//        final List<SeatEntity> seatEntities = seatRepository.findByPerformanceId(performanceId);
+//        if (seatEntities.isEmpty()) {
+//            throw new NotFoundException(ErrorType.NOT_FOUND);
+//        }
+//        return seatEntities.stream()
+//                .map(e -> new Seat(e.getId(),
+//                        e.getPerformanceId(),
+//                        e.getX(),
+//                        e.getY(),
+//                        e.getStatus())
+//                )
+//                .toList();
+//    }
 }
