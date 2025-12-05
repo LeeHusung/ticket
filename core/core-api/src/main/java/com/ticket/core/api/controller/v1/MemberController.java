460dc3725b0967e931de3eca5d479c18660567e1
HuSung <128116509+LeeHusung@users.noreply.github.com>
2025-12-05 13:41:25 +0900

    feat(auth): 로그인 시 세션 생성 후 attribute 세팅, HttpSession 조회 기능 추가

--- core/core-api/src/main/java/com/ticket/core/api/controller/v1/MemberController.java
+++ core/core-api/src/main/java/com/ticket/core/api/controller/v1/MemberController.java
@@ -6,12 +6,16 @@ import com.ticket.core.api.controller.v1.response.MemberResponse;
 import com.ticket.core.domain.member.Member;
 import com.ticket.core.domain.member.MemberService;
 import com.ticket.core.support.response.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/member")
 public class MemberController {
+
+    private static final String LOGIN_MEMBER = "LOGIN_MEMBER";
 
     private final MemberService memberService;
 
@@ -25,14 +29,18 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public ApiResponse<MemberResponse> login(@RequestBody @Valid LoginMemberRequest request) {
+    public ApiResponse<MemberResponse> login(@RequestBody @Valid LoginMemberRequest request, HttpServletRequest httpRequest) {
         final Member loginedMember = memberService.login(request.getEmail(), request.getPassword());
+
+        final HttpSession session = httpRequest.getSession();
+        session.setAttribute(LOGIN_MEMBER, loginedMember);
         return ApiResponse.success(new MemberResponse(loginedMember.getId(), loginedMember.getEmail().getEmail(), loginedMember.getName()));
     }
 
     @GetMapping("/{memberId}")
-    public ApiResponse<MemberResponse> findById(@PathVariable Long memberId) {
+    public ApiResponse<MemberResponse> findById(@PathVariable Long memberId, HttpSession session) {
         final Member findMember = memberService.findById(memberId);
+        final Member member = (Member) session.getAttribute(LOGIN_MEMBER);
         return ApiResponse.success(new MemberResponse(findMember.getId(), findMember.getEmail().getEmail(), findMember.getName()));
     }
 }
