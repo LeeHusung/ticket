02665ad04c02c375e11b8837c9c02b2c523d3fe2
mn040820 <mn040820@naver.com>
2025-12-10 20:09:02 +0900

    test(password): 비밀번호가 null이거나 공백이거나 4자 미만이면 예외를 터트린다.

---
+++ core/core-api/src/test/java/com/ticket/core/domain/member/vo/PasswordTest.java
@@ -0,0 +1,19 @@
+package com.ticket.core.domain.member.vo;
+
+import com.ticket.core.support.exception.CoreException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@SuppressWarnings("NonAsciiCharacters")
+class PasswordTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "123", "abc"})
+    void 비밀번호가_null이거나_공백이거나_4자_미만이면_실패한다(final String value) {
+        //then
+        Assertions.assertThatThrownBy(() -> new Password(value)).isInstanceOf(CoreException.class);
+    }
+}
