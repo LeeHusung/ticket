8dd37b4f0ac579ed8f77718bb0566f3c1374c24e
HuSung <128116509+LeeHusung@users.noreply.github.com>
2025-12-15 10:04:15 +0900

    feat(reservation): 예매 기능 리팩토링 중(임시 커밋)

--- storage/db-core/src/main/java/com/ticket/storage/db/core/PerformanceRepository.java
+++ storage/db-core/src/main/java/com/ticket/storage/db/core/PerformanceRepository.java
@@ -1,6 +1,11 @@
 package com.ticket.storage.db.core;
 
+import com.ticket.core.enums.EntityStatus;
+import com.ticket.core.enums.PerformanceState;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PerformanceRepository extends JpaRepository<PerformanceEntity, Long> {
+    Optional<PerformanceEntity> findByIdAndShowIdAndStateAndStatus(Long performanceId, Long showId, PerformanceState state, EntityStatus status);
 }
