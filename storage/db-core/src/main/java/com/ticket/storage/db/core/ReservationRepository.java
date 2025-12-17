9348db11da6886b1b6df54aebbcda51b789b1343
HuSung <128116509+LeeHusung@users.noreply.github.com>
2025-12-17 09:01:27 +0900

    refactor(reservation): JPQL 사용으로 쿼리 호출 수 감소(2 -> 1)

--- storage/db-core/src/main/java/com/ticket/storage/db/core/ReservationRepository.java
+++ storage/db-core/src/main/java/com/ticket/storage/db/core/ReservationRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<ReservationEntity, Long> {
-    List<ReservationEntity> findAllByMemberIdAndPerformanceId(Long memberId, Long performanceId);
 }
