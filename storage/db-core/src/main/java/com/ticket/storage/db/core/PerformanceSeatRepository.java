37bc9875091875900117432c5eb16ab5a4b3ed5e
HuSung <128116509+LeeHusung@users.noreply.github.com>
2025-12-23 08:48:10 +0900

    feat(reservation): PerformanceSeat에 비관적 락 적용

--- storage/db-core/src/main/java/com/ticket/storage/db/core/PerformanceSeatRepository.java
+++ storage/db-core/src/main/java/com/ticket/storage/db/core/PerformanceSeatRepository.java
@@ -1,10 +1,13 @@
 package com.ticket.storage.db.core;
 
 import com.ticket.core.enums.PerformanceSeatState;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.List;
 
 public interface PerformanceSeatRepository extends JpaRepository<PerformanceSeatEntity, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<PerformanceSeatEntity> findByPerformanceIdAndSeatIdInAndState(Long performanceId, List<Long> seatIds, PerformanceSeatState state);
 }
