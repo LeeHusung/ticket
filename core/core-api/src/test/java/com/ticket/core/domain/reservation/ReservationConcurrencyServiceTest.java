b69ca6dc0d7213859b42ee1530cc74860b0fb0c3
HuSung <128116509+LeeHusung@users.noreply.github.com>
2025-12-23 08:13:36 +0900

    test(reservation): 예매 동시 요청 시 oversell 확인 테스트

---
+++ core/core-api/src/test/java/com/ticket/core/domain/reservation/ReservationConcurrencyServiceTest.java
@@ -0,0 +1,89 @@
+package com.ticket.core.domain.reservation;
+
+import com.ticket.core.domain.member.MemberFinder;
+import com.ticket.core.enums.Role;
+import com.ticket.core.support.TestDataFactory;
+import com.ticket.storage.db.core.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Import({ReservationService.class, MemberFinder.class, ReservationValidator.class, ReservationManager.class})
+@SuppressWarnings("NonAsciiCharacters")
+class ReservationConcurrencyServiceTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ReservationConcurrencyServiceTest.class);
+    @Autowired private ReservationService reservationService;
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private PerformanceRepository performanceRepository;
+    @Autowired private PerformanceSeatRepository performanceSeatRepository;
+    @Autowired private ReservationRepository reservationRepository;
+    @Autowired private ReservationDetailRepository reservationDetailRepository;
+
+    private MemberEntity savedMember;
+    private List<MemberEntity> saveMembers;
+    private PerformanceEntity savedPerformance;
+    private List<PerformanceSeatEntity> savedPerformanceSeats;
+
+    //todo 이거 미리 테스트 db에 데이터 세팅해놓고 테스트하자. 매번 생성하는 것 보다 나을듯.
+    @BeforeEach
+    void setUp() {
+        savedMember = memberRepository.save(TestDataFactory.createMember());
+        savedPerformance = performanceRepository.save(TestDataFactory.createPerformance());
+        savedPerformanceSeats = performanceSeatRepository.saveAll(
+                TestDataFactory.createAvailableSeats(savedPerformance.getId(), List.of(1L))
+        );
+        saveMembers = IntStream.range(0, 100)
+                .mapToObj(i -> memberRepository.save(TestDataFactory.createMember("user" + i + "@test.com", "pw", "name", Role.MEMBER)))
+                .toList();
+    }
+
+    @Test
+    void 재고가_1개일때_여러_요청이_동시에_들어오면_예매가_오버셀된다() throws InterruptedException {
+        // given
+        final int threadCount = 100;
+        final ExecutorService es = Executors.newFixedThreadPool(32);
+        final CountDownLatch latch = new CountDownLatch(threadCount);
+        AtomicInteger successCount = new AtomicInteger(0);
+        AtomicInteger failCount = new AtomicInteger(0);
+
+        for (int i = 0; i < threadCount; i++) {
+            int idx = i;
+            es.submit(() -> {
+                try {
+                    final NewReservation request = new NewReservation(
+                            saveMembers.get(idx).getId(),
+                            savedPerformance.getId(),
+                            List.of(1L)
+                    );
+                    reservationService.addReservation(request);
+                    successCount.incrementAndGet();
+                } catch (Exception e) {
+                    log.error("예약 실패: {}", e.getMessage(), e);
+                    failCount.incrementAndGet();
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        System.out.println("result: " + successCount.get() + " / " + failCount.get());
+        assertThat(successCount.get()).isEqualTo(1);
+        final long reservationCount = reservationRepository.count();
+        assertThat(reservationCount).isEqualTo(1);
+    }
+}
