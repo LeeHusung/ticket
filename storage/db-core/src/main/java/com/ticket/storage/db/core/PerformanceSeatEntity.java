ce5416e02c9ea1d76710e49bb85fe6123d8824d8
HuSung <128116509+LeeHusung@users.noreply.github.com>
2025-12-31 08:47:46 +0900

    feat(performanceSeat): 회차좌석 엔티티에 선점 관련 컬럼 추가

--- storage/db-core/src/main/java/com/ticket/storage/db/core/PerformanceSeatEntity.java
+++ storage/db-core/src/main/java/com/ticket/storage/db/core/PerformanceSeatEntity.java
@@ -3,6 +3,8 @@ package com.ticket.storage.db.core;
 import com.ticket.core.enums.PerformanceSeatState;
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "PERFORMANCE_SEAT")
 public class PerformanceSeatEntity extends BaseEntity {
@@ -17,12 +19,21 @@ public class PerformanceSeatEntity extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private PerformanceSeatState state;
 
+    private LocalDateTime holdExpireAt;
+
+    private Long holdByMemberId;
+
+    private String holdToken;
+
     protected PerformanceSeatEntity() {}
 
-    public PerformanceSeatEntity(final Long performanceId, final Long seatId, final PerformanceSeatState state) {
+    public PerformanceSeatEntity(final Long performanceId, final Long seatId, final PerformanceSeatState state, final LocalDateTime holdExpireAt, final Long holdByMemberId, final String holdToken) {
         this.performanceId = performanceId;
         this.seatId = seatId;
         this.state = state;
+        this.holdExpireAt = holdExpireAt;
+        this.holdByMemberId = holdByMemberId;
+        this.holdToken = holdToken;
     }
 
     public Long getId() {
@@ -41,11 +52,29 @@ public class PerformanceSeatEntity extends BaseEntity {
         return state;
     }
 
+    public LocalDateTime getHoldExpireAt() {
+        return holdExpireAt;
+    }
+
+    public Long getHoldByMemberId() {
+        return holdByMemberId;
+    }
+
+    public String getHoldToken() {
+        return holdToken;
+    }
+
     public void reserve() {
         this.state = PerformanceSeatState.RESERVED;
     }
 
-    public void hold() {
+    public void hold(final Integer holdTime, final String holdToken) {
         this.state = PerformanceSeatState.HELD;
+        this.holdExpireAt = LocalDateTime.now().plusSeconds(holdTime);
+        this.holdToken = holdToken;
+    }
+
+    public void release() {
+        this.state = PerformanceSeatState.AVAILABLE;
     }
 }
